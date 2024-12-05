5ce176cb3039aab3bfe7555c190390afb9f20cae
Albert Parti <info@appsyscode.com>
2024-12-05 18:58:51 +0100

    Initial Commit

---
+++ APPlainAlertSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APPlainAlertSwift
+//
+//  Created by Parti Albert on 2024. 12. 05..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
